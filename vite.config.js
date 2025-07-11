a06a8f6d4324137f779050767143c4b5bd32ce3a
Sehan Diki Triansyah <sehandiki17@gmail.com>
2025-07-11 17:49:25 +0700

    Fix Vite config and index.html for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal_portfolio/",
+  base: "/",
   build: {
     outDir: 'dist',
     sourcemap: true,
+  },
+  server: {
+    port: 3000,
   },
 });
